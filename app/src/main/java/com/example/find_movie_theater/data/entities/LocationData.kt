f641ec1a43dd368f7aaacd9f3b289e2323654450
tmdrl980430 <tmdrl980430@naver.com>
2023-12-22 17:58:21 +0900

    12/22 퇴근

---
+++ app/src/main/java/com/example/find_movie_theater/data/entities/LocationData.kt
@@ -0,0 +1,8 @@
+package com.example.find_movie_theater.data.entities
+
+import com.google.gson.annotations.SerializedName
+
+data class LocationData(
+    @SerializedName("latitude") val latitude: Double?,
+    @SerializedName("longitude") val longitude: Double?,
+)
--- app/src/main/java/com/example/find_movie_theater/ui/main/home/HomeFragment.kt
+++ app/src/main/java/com/example/find_movie_theater/ui/main/home/HomeFragment.kt
@@ -1,82 +1,114 @@
 package com.example.find_movie_theater.ui.main.home
 
-import android.util.Log
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import com.example.find_movie_theater.R
+import com.example.find_movie_theater.data.entities.LocationData
 import com.example.find_movie_theater.databinding.FragmentHomeBinding
 import com.example.find_movie_theater.ui.BaseFragment
 import com.naver.maps.geometry.LatLng
-import com.naver.maps.map.CameraUpdate
 import com.naver.maps.map.LocationTrackingMode
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.NaverMap
-import com.naver.maps.map.NaverMapSdk
 import com.naver.maps.map.OnMapReadyCallback
 import com.naver.maps.map.overlay.Marker
 import com.naver.maps.map.util.FusedLocationSource
-
+import java.util.Locale
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate),
     OnMapReadyCallback {
 
     private val LOCATION_PERMISSION_REQUEST_CODE = 5000
-    private val marker = Marker()
-
-
+    private val PERMISSIONS = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    )
 
     private lateinit var naverMap: NaverMap
     private lateinit var locationSource: FusedLocationSource
+    private val marker = Marker()
 
     override fun initAfterBinding() {
-        // 네이버 지도 SDK 초기화
-        NaverMapSdk.getInstance(requireContext()).client =
-            NaverMapSdk.NaverCloudPlatformClient("tn8gmubytk")
+        if (!hasPermission()) {
+            requestPermissions(PERMISSIONS, LOCATION_PERMISSION_REQUEST_CODE)
+        } else {
+            initMapView()
+        }
+    }
 
-        // MapFragment를 사용하여 네이버 지도를 Fragment에 추가
-        val mapFragment = childFragmentManager.findFragmentById(R.id.map_container) as MapFragment?
+    private fun initMapView() {
+        val fm = childFragmentManager
+        val mapFragment = fm.findFragmentById(R.id.map_container) as MapFragment?
             ?: MapFragment.newInstance().also {
-                childFragmentManager.beginTransaction()
-                    .add(R.id.map_container, it)
-                    .commit()
+                fm.beginTransaction().add(R.id.map_container, it).commit()
             }
 
-        // fragment의 getMapAsync() 메서드로 OnMapReadyCallback 콜백을 등록하면 비동기로 NaverMap 객체를 얻을 수 있습니다.
         mapFragment.getMapAsync(this)
-
-        // FusedLocationSource 초기화
         locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
     }
 
-    // onMapReady에서 네이버 지도가 준비되었을 때 호출되는 콜백에서 현재 위치 설정 및 기능 추가
+    private fun hasPermission(): Boolean {
+        return PERMISSIONS.all {
+            ContextCompat.checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+
     override fun onMapReady(naverMap: NaverMap) {
         this.naverMap = naverMap
-
-        // 현재 위치를 표시하기 위해 FusedLocationSource를 사용합니다.
         naverMap.locationSource = locationSource
-
-        // 현재 위치 버튼 활성화
         naverMap.uiSettings.isLocationButtonEnabled = true
-
-        // 위치를 추적하면서 카메라도 따라 움직이도록 설정
         naverMap.locationTrackingMode = LocationTrackingMode.Follow
 
-        // 지도가 클릭 되면 onMapClick() 콜백 메서드가 호출 되며, 파라미터로 클릭된 지점의 화면 좌표와 지도 좌표가 전달 된다.
+        val locationList: ArrayList<LocationData> = arrayListOf(
+            LocationData(126.897348, 37.5682507),
+            LocationData(126.941194, 37.5597128),
+            LocationData(126.922071, 37.5560525)
+        )
+
+//        val markers = Array<Marker?>(locationList.size) { null }
+//
+//        locationList.forEachIndexed { index, locationData ->
+//            markers[index] = Marker().apply {
+//                addMarker(locationData.latitude ?: 0.0, locationData.longitude ?: 0.0)
+//                setOnClickListener {
+//                    Toast.makeText(requireActivity(), "마커 $index 클릭", Toast.LENGTH_SHORT).show()
+//                    false
+//                }
+//            }
+//        }
+
+
+
         naverMap.setOnMapClickListener { point, coord ->
-            Log.d("this", "${coord.latitude}, ${coord.longitude}")
-            marker.position = LatLng(coord.latitude, coord.longitude)
-            marker.map = naverMap
-
-
+            addMarker(coord.latitude, coord.longitude)
         }
+    }
 
-        // 지도가 롱 클릭 되면 onMapLongClick() 콜백 메서드가 호출 되며, 파라미터로 클릭된 지점의 화면 좌표와 지도 좌표가 전달 된다.
-        naverMap.setOnMapLongClickListener { point, coord ->
+    private fun addMarker(latitude: Double, longitude: Double) {
+        marker.position = LatLng(latitude, longitude)
+        marker.map = naverMap
+        getAddressInBackground(latitude, longitude)
+    }
 
-            Log.d("this", "${coord.latitude}, ${coord.longitude}")
+
+    private fun getAddressInBackground(latitude: Double, longitude: Double) {
+        Thread {
+            val geocoder = Geocoder(requireContext(), Locale.KOREAN)
+            val addresses = geocoder.getFromLocation(latitude, longitude, 1)
+
+            if (addresses != null && addresses.isNotEmpty()) {
+                val address = addresses[0].getAddressLine(0)
+                toast(address)
+            }
+        }.start()
+    }
+
+    private fun toast(text: String) {
+        requireActivity().runOnUiThread {
+            Toast.makeText(requireContext(), text, Toast.LENGTH_SHORT).show()
         }
-
-        // 네이버 지도가 준비되면 현재 위치를 표시하는 코드를 추가할 수 있습니다.
-        // 예를 들어, 서울 시청으로 카메라 이동
-        val seoul = LatLng(37.5666102, 126.9783881)
-        naverMap.moveCamera(CameraUpdate.scrollTo(seoul))
     }
 }
